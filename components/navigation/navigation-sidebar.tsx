b0630dd38af74f91c5ac19d01592982059e7b276
Martin-Hayot <m.hayot@students.ephec.be>
2024-03-02 15:24:13 +0100

    feat: admin link on sidebar nav

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -10,6 +10,12 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ChevronDown, ChevronUp, Shield } from "lucide-react";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuTrigger,
+} from "../ui/navigation-menu";
 
 const NavigationSidebar = ({ setOpen }: { setOpen: Function }) => {
     const user = useCurrentUser();
@@ -65,26 +71,12 @@ const NavigationSidebar = ({ setOpen }: { setOpen: Function }) => {
                     Register
                 </Link>
                 {user?.role === "ADMIN" && (
-                    <DropdownMenu>
-                        <DropdownMenuTrigger>
-                            <div className="hover:text-accent dark:hover:text-accent duration-150">
-                                <Shield className="w-8 h-8 inline mr-2 mb-2" />
-                                Admin
-                                <ChevronDown className="ml-2 w-6 h-6 inline" />
-                            </div>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent
-                            align="center"
-                            className="dark:bg-white dark:text-black ml-32"
-                        >
-                            <DropdownMenuItem className="hover:bg-gray-200 focus:bg-gray-200">
-                                Create Offers
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className="hover:bg-gray-200 focus:bg-gray-200">
-                                Car Models
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                    <Link href="/admin/dashboard">
+                        <div className="hover:text-accent dark:hover:text-accent duration-150">
+                            <Shield className="w-8 h-8 inline mr-2 mb-2" />
+                            Admin
+                        </div>
+                    </Link>
                 )}
             </nav>
         </div>
