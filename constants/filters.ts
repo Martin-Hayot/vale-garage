9b23d5b9116e0bc2ac13788bc9ffb4042caa7e46
Martin-Hayot <m.hayot@students.ephec.be>
2024-04-03 19:49:19 +0200

    feat: save state to url hook

--- constants/filters.ts
+++ constants/filters.ts
@@ -1,6 +1,27 @@
+import {
+    CAR_COLORS,
+    TRANSMISSION_TYPES,
+    CAR_BODIES,
+    CAR_STATES,
+    FUEL_TYPES,
+    GEARBOX_TYPES,
+} from "./cars";
+
 export const SORT_OPTIONS = [
     { label: "Newest", value: "newest" },
     { label: "Oldest", value: "oldest" },
     { label: "Price: Low to High", value: "price_asc" },
     { label: "Price: High to Low", value: "price_desc" },
 ] as const;
+
+export const COLOR_OPTIONS = ["any", ...CAR_COLORS] as const;
+
+export const TRANSMISSION_OPTIONS = ["any", ...TRANSMISSION_TYPES] as const;
+
+export const BODY_OPTIONS = ["any", ...CAR_BODIES] as const;
+
+export const STATE_OPTIONS = ["any", ...CAR_STATES] as const;
+
+export const FUEL_OPTIONS = ["any", ...FUEL_TYPES] as const;
+
+export const GEARBOX_OPTIONS = ["any", ...GEARBOX_TYPES] as const;
--- store/filters.ts
+++ store/filters.ts
@@ -1,13 +1,40 @@
 import { create } from "zustand";
-import { SORT_OPTIONS } from "@/constants/filters";
+import {
+    SORT_OPTIONS,
+    COLOR_OPTIONS,
+    BODY_OPTIONS,
+    GEARBOX_OPTIONS,
+    TRANSMISSION_OPTIONS,
+    STATE_OPTIONS,
+    FUEL_OPTIONS,
+} from "@/constants/filters";
 
 type SORT_OPTIONS = (typeof SORT_OPTIONS)[number]["value"];
+type COLOR_OPTIONS = (typeof COLOR_OPTIONS)[number];
+type BODY_OPTIONS = (typeof BODY_OPTIONS)[number];
+type GEARBOX_OPTIONS = (typeof GEARBOX_OPTIONS)[number];
+type TRANSMISSION_OPTIONS = (typeof TRANSMISSION_OPTIONS)[number];
+type STATE_OPTIONS = (typeof STATE_OPTIONS)[number];
+type FUEL_OPTIONS = (typeof FUEL_OPTIONS)[number];
 
-type Filters = {
+export type Filters = {
     sort: SORT_OPTIONS;
+    color: COLOR_OPTIONS;
+    body: BODY_OPTIONS;
+    gearbox: GEARBOX_OPTIONS;
+    transmission: TRANSMISSION_OPTIONS;
+    state: STATE_OPTIONS;
+    fuel: FUEL_OPTIONS;
 };
 
 export const useFilters = create<Filters>((set) => ({
     sort: "newest",
-    setSort: (sort: SORT_OPTIONS) => set({ sort }),
+    color: COLOR_OPTIONS[0],
+    body: BODY_OPTIONS[0],
+    gearbox: GEARBOX_OPTIONS[0],
+    transmission: TRANSMISSION_OPTIONS[0],
+    state: STATE_OPTIONS[0],
+    fuel: FUEL_OPTIONS[0],
+    setFilter: (filter: keyof Filters, value: any) => set({ [filter]: value }),
+    setFilters: (filters: Partial<Filters>) => set(filters),
 }));
