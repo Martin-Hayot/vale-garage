9fa1359fb2de3812994b5884b946141123c31d3b
Martin-Hayot <m.hayot@students.ephec.be>
2024-07-10 13:19:51 +0200

    feat: first iteration merchants api route

--- app/api/merchants/route.ts
+++ app/api/merchants/route.ts
@@ -2,4 +2,19 @@ import { currentUser } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {}
+export async function POST(req: Request) {
+    const user = await currentUser();
+
+    if (!user || user?.role === "MERCHANT") {
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const merchant = await db.user.update({
+        where: {
+            id: user.id,
+        },
+        data: {},
+    });
+}
