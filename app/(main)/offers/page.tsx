93b41560902bb0d7f0135d169515142308e60939
Martin-Hayot <m.hayot@students.ephec.be>
2024-12-10 00:10:13 +0100

    feat: auction tab wip

--- app/(main)/offers/page.tsx
+++ app/(main)/offers/page.tsx
@@ -3,24 +3,18 @@
 import OffersCard from "@/components/offers/offers-card";
 import OffersSkeleton from "@/components/offers/offers-skeleton";
 
-import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-} from "@/components/ui/pagination";
-
 import { useFilters } from "@/store/filters";
-import { Car, Sales, OfferImages } from "@prisma/client";
+import { Car, Sales, OfferImages, Auctions } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
 import { gsap } from "gsap";
 import { useEffect, useRef, useState } from "react";
 import OffersSidebar from "@/components/offers/offers-sidebar";
-import { useRouter, useSearchParams } from "next/navigation";
+import {
+    ReadonlyURLSearchParams,
+    useRouter,
+    useSearchParams,
+} from "next/navigation";
 import { useDrawer } from "@/store/drawer";
 import {
     MILEAGE_OPTIONS,
@@ -36,6 +30,8 @@ import { useWebSocketStore } from "@/store/websocket";
 import OffersPagination from "@/components/offers/offers-pagination";
 
 type CarSales = Sales & { car: Car } & { offerImages: OfferImages[] };
+
+type CarAuctions = Auctions & { car: Car } & { offerImages: OfferImages[] };
 
 type validFuels =
     | "Diesel"
@@ -211,15 +207,7 @@ const SalesPage = () => {
     return (
         <div>
             <MainBar tab={tab} setTab={setTab} />
-            {tab === "auctions" && (
-                <div>
-                    {isConnected ? (
-                        <div>Connected to WebSocket</div>
-                    ) : (
-                        <div>Not connected to WebSocket</div>
-                    )}
-                </div>
-            )}
+            {tab === "auctions" && <AuctionTab searchParams={searchParams} />}
             {tab === "sales" && (
                 <div className="w-full flex flex-row justify-center gap-x-2 mb-64 mt-8">
                     <div>
@@ -289,6 +277,134 @@ const SalesPage = () => {
                     </div>
                 </div>
             )}
+        </div>
+    );
+};
+
+interface AuctionTabProps {
+    searchParams: ReadonlyURLSearchParams;
+}
+
+const AuctionTab = ({ searchParams }: AuctionTabProps) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 10;
+
+    type response = {
+        data: CarAuctions[];
+        meta: {
+            totalItems: number;
+            currentPage: number;
+            totalPages: number;
+        };
+    };
+
+    const {
+        data: offers,
+        isPending,
+        isLoading,
+        error,
+    } = useQuery<response, AxiosError>({
+        queryKey: ["auctions", searchParams.toString(), currentPage],
+        queryFn: async () => {
+            const url = new URL(window.location.href);
+            const searchParams = new URLSearchParams(url.search);
+            const params = Object.fromEntries(searchParams);
+            params.status = "ACTIVE";
+            params.page = currentPage.toString();
+            params.limit = itemsPerPage.toString();
+            const { data } = await axios.get("/api/auctions", {
+                params,
+            });
+
+            return data;
+        },
+    });
+
+    const handlePageChange = (newPage: number) => {
+        setCurrentPage(newPage);
+    };
+
+    const skeletonRef = useRef(null);
+    const dataRef = useRef(null);
+
+    useEffect(() => {
+        if (!isPending && !isLoading) {
+            gsap.to(skeletonRef.current, { opacity: 0, duration: 1 });
+            gsap.fromTo(
+                dataRef.current,
+                { opacity: 0 },
+                { opacity: 1, duration: 1 }
+            );
+        } else {
+            gsap.to(skeletonRef.current, { opacity: 1, duration: 1 });
+            gsap.to(dataRef.current, { opacity: 0, duration: 1 });
+        }
+    }, [offers, isLoading, isPending]);
+
+    return (
+        <div className="w-full flex flex-row justify-center gap-x-2 mb-64 mt-8">
+            <div>
+                <OffersSidebar />
+            </div>
+            <div className="flex flex-row justify-center items-start w-full basis-1/2">
+                <div className="w-full max-w-[1420px]">
+                    <div>
+                        <div className="flex flex-row justify-end items-end pb-4 pr-8">
+                            <div className="font-semibold text-2xl">
+                                {offers?.data.length ?? "..."} car
+                                {offers?.data.length && offers.data.length >= 1
+                                    ? ""
+                                    : "s"}{" "}
+                                found
+                            </div>
+                        </div>
+                        <div
+                            className="offers-grid gap-6 mx-4 ml-20"
+                            style={{ opacity: 1 }}
+                            ref={skeletonRef}
+                        >
+                            {isPending &&
+                                new Array(12)
+                                    .fill(null)
+                                    .map((_, index) => (
+                                        <OffersSkeleton key={index} />
+                                    ))}
+                        </div>
+                        {error && (
+                            <div className="flex justify-center text-4xl my-48">
+                                Error: {error.message}
+                            </div>
+                        )}
+                        <div className="offers-grid gap-6 mx-4" ref={dataRef}>
+                            {offers &&
+                                !isPending &&
+                                offers.data.map((offer) => (
+                                    <div
+                                        className="place-self-center"
+                                        key={offer.id}
+                                    >
+                                        <OffersCard
+                                            details={{
+                                                ...offer.car,
+                                                ...offer,
+                                                price: offer.currentBid,
+                                            }}
+                                        />
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                    <div className="mt-40">
+                        <OffersPagination
+                            currentPage={currentPage}
+                            totalPages={offers?.meta?.totalPages}
+                            handlePageChange={handlePageChange}
+                            itemsPerPage={itemsPerPage}
+                            dataLength={offers?.data.length}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
